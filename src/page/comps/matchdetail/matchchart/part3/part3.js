cdf4051d07b63e35d6183d44f111fadec684cfd8
siniangy <1198246001@qq.com>
2018-12-29 10:33:04 +0800

    12/29

--- src/page/comps/matchdetail/matchchart/part3/part3.js
+++ src/page/comps/matchdetail/matchchart/part3/part3.js
@@ -168,19 +168,19 @@ export default class Part3 extends React.Component {
 		return (
 			<div style={{border: '2px solid rgba(240,242,245,1)',borderRadius: '10px',padding: '10px',minHeight:'150px',marginTop: '20px'}}>
 				<Row>
-					<Col span={16} style={{paddingRight: '20px',paddingTop: '20px'}} id='part3'>
-						<h3>球队助攻关系</h3>
+					<Col span={12} style={{paddingRight: '20px',paddingTop: '20px'}}>
+						<h3>投篮热点分析</h3>
+						<Chart3 teamName={this.state.team2Name} teamNum={defaultValue}/>
+					</Col>
+					<Col span={12} style={{paddingRight: '20px',paddingTop: '20px'}} id='part3'>
+						<h3 style={{}}>球队助攻关系</h3>
 						<span>
-							<RadioGroup onChange={(e) => this.handleButton(e)} defaultValue="a">
+							<RadioGroup onChange={(e) => this.handleButton(e)} defaultValue="a" style={{}}>
 	           	 	<RadioButton value="a">{team1Name}</RadioButton>
 	            	<RadioButton value="b">{team2Name}</RadioButton>
 	          	</RadioGroup>
 						</span>
 						<Chart1 players={(defaultValue=='a') ? team1Players : team2Players} relation={(defaultValue=='a') ? team1Relation : team2Relation} width={this.state.width}/>
-					</Col>
-					<Col span={8} style={{paddingRight: '20px',paddingTop: '20px'}}>
-						<h3>投篮热点分析</h3>
-						<Chart3 teamName={this.state.team2Name} teamNum={defaultValue}/>
 					</Col>
 				</Row>
       </div>
